edebc004f1dfd2f561123145a267ba46154be35c
vr-greycube <vijay@greycube.in>
2022-10-11 14:31:51 +0530

    fix: typo in module name

--- amazon_sp_erpnext/amazon_sp_erpnext/doctype/amazon_sp_settings/amazon_sp_settings.js
+++ amazon_sp_erpnext/amazon_sp_erpnext/doctype/amazon_sp_settings/amazon_sp_settings.js
@@ -5,7 +5,7 @@ frappe.ui.form.on("Amazon SP Settings", {
   fetch_report: function (frm) {
     frappe.call({
       method:
-        "amazon_sp_erpnext.amazon_sp_erpnext.controllers.report_controller.fetch_report",
+        "amazon_sp_erpnext.amazon_sp_erpnext.controllers.reports_controller.fetch_report",
       args: {
         report_type: frm.doc.report_type,
         amz_settings_name: frm.doc.name,
