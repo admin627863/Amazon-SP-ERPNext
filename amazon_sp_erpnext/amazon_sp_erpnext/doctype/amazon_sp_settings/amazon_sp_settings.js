f394eec80bc143023edb5e14094c0468fb18909a
vr-greycube <vijay@greycube.in>
2022-10-11 14:27:10 +0530

    minor: amazon report

--- amazon_sp_erpnext/amazon_sp_erpnext/doctype/amazon_sp_settings/amazon_sp_settings.js
+++ amazon_sp_erpnext/amazon_sp_erpnext/doctype/amazon_sp_settings/amazon_sp_settings.js
@@ -3,12 +3,16 @@
 
 frappe.ui.form.on("Amazon SP Settings", {
   fetch_report: function (frm) {
-    open_url_post(
-      "/api/method/amazon_sp_erpnext.amazon_sp_erpnext.controllers.report_controller.fetch_report",
-      {
+    frappe.call({
+      method:
+        "amazon_sp_erpnext.amazon_sp_erpnext.controllers.report_controller.fetch_report",
+      args: {
         report_type: frm.doc.report_type,
         amz_settings_name: frm.doc.name,
-      }
-    );
+      },
+      callback: () => {
+        console.log("file created");
+      },
+    });
   },
 });
